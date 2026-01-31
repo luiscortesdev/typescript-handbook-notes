7f775d937ae936c956f14d96f5054a9ad9764605
luiscortesdev <luiscortesdev@gmail.com>
2026-01-31 15:21:41 -0500

    Added typesbyinference code snippet

---
+++ typescriptforjsprogrammers/typesbyinference.ts
@@ -0,0 +1,5 @@
+let helloWorld = "Hello World";
+
+// helloWorld() Returns an error because Typescript already inferred helloWorld's type
+
+console.log(helloWorld.toLowerCase()) // This works though
