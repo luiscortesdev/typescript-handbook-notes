1c99bb6b6c26f5d2ca2a9587fd25d9067f1c0313
luiscortesdev <luiscortesdev@gmail.com>
2026-02-02 14:47:45 -0500

    Updated typeguards.ts in narrowing

--- narrowing/typeguards.ts
+++ narrowing/typeguards.ts
@@ -1,10 +1,16 @@
-// There are a few ways we can narrow down our types
+// We can narrow down our types using the typeof function.
 
-function printAll(strs: string | string[] | null) { 
+function printAll(strs: string | string[] | null) {
+    // However, we still need to be careful with our type guards.
     if (typeof strs === "object") {
         // This check fails because, in JavaScript and TypeScript, null is an object.
+        // TypeScript warns us of this.
         for (const s of strs) {
             console.log(s)
         }
+    } else if (typeof strs === "string") {
+        console.log(strs);
+    } else {
+        // do nothing
     }
 }
