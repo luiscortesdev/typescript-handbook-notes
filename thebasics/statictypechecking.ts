c0ad69394ecb7902ddf714a56a01d320125d4153
luiscortesdev <luiscortesdev@gmail.com>
2026-02-01 19:52:34 -0500

    Added statictypechecking

---
+++ thebasics/statictypechecking.ts
@@ -0,0 +1,4 @@
+// TypeScript notifies the programmer of errors before without the code even having to run.
+
+const goodMorning = "Good Morning"
+goodMorning() // Throws an error without the code even running
