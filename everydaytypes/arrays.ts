3f6877f0d8c0a19501a7f930d3360be015acbd4d
luiscortesdev <luiscortesdev@gmail.com>
2026-02-02 11:09:24 -0500

    Updated primitives

---
+++ everydaytypes/arrays.ts
@@ -0,0 +1,3 @@
+// You can add square brackets to the end of a primitive to define an array of that primitive
+
+let myStrings: string[] = ["hi", "hello", "welcome"]
---
+++ everydaytypes/primitives.ts
@@ -0,0 +1,7 @@
+// TypeScript uses the types you see in JavaScript. Very easy to understand types.
+
+let name: string = "Juan" // string values for string type
+let myNum: number = 2 // TypeScript uses number for both integers and floats. JavaScript doesn't separate the two.
+let myFloat: number = 5.4
+let myBool: boolean = true // boolean for bool values
+
