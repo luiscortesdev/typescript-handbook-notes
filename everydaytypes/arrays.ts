607c99cf1bc94cb3c164ab3458beaa0b2ea044be
luiscortesdev <luiscortesdev@gmail.com>
2026-02-02 11:18:04 -0500

    Added typeannotations.ts

--- everydaytypes/arrays.ts
+++ everydaytypes/arrays.ts
@@ -1,3 +1,16 @@
 // You can add square brackets to the end of a primitive to define an array of that primitive
+// ex. string[], number[]
+// You can also use Array<primitve> to define an array of primitives
 
 let myStrings: string[] = ["hi", "hello", "welcome"]
+let myNumbers: Array<number> = [1, 2, 3, 4, 5]
+
+// TypeScript has a special type called any that can be assigned to values.
+// However, TypeScript won't do any of its typechecking.
+
+let obj: any = { x: 0 }
+obj.foo() // TypeScript won't throw an error even though obj doesn't have a method called foo
+
+// TypeScript will let you assign a variable of a number type to obj even though you probably
+// don't want this.
+const n: number = obj
---
+++ everydaytypes/typeannotations.ts
@@ -0,0 +1,12 @@
+// As you have probably noticed, I have been annotating my variables with types
+
+// Like this:
+let n: number = 0
+
+// This isn't really needed most of the time (except for making it easier for others to understand)
+// as TypeScript can infer the types of new primitive variables.
+
+// TypeScript always puts type annotations after the variable being typed
+let myBool: boolean = true
+// not
+// bool myBool = true
