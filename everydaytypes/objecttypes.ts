842ae4da1bbf32ba13a5a2168b04f93dda8fdbb0
luiscortesdev <luiscortesdev@gmail.com>
2026-02-02 11:43:51 -0500

    Object types finished

--- everydaytypes/objecttypes.ts
+++ everydaytypes/objecttypes.ts
@@ -6,7 +6,11 @@ function printCoord(pt: { x: number; y: number }) {
 printCoord({ x: 3, y: 7 });
 
 // Some object properties might not always exist. We can define these as optional using a "?".
+// If we try to access that property, TypeScript will force us to make sure it isn't undefined.
 function printFullName(person: {firstName: string; middleName?: string; lastName: string}) { 
     console.log(person.firstName, person.middleName, person.lastName)
+    // console.log(person.middleName.toUpperCase()) Errors because middleName could be undefined.
+    console.log(person.middleName?.toUpperCase()) // Modern way to ensure the property exists.
 }
 printFullName({firstName: "Bob", lastName: "Guy"}) // Won't error even though there isn't a middle name.
+
