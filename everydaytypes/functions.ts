3de4a52fa73248a19ae871496f2306a357a6ef26
luiscortesdev <luiscortesdev@gmail.com>
2026-02-02 11:36:33 -0500

    Added functions.ts

---
+++ everydaytypes/functions.ts
@@ -0,0 +1,24 @@
+// Parameters can be given type annotations
+function greet(name: string) {
+    console.log(`Hello, ${name}`)
+}
+
+greet(24) // Throws an error
+
+
+// Function return values can also be given types
+function randomNumber(): number {
+    return Math.random()
+}
+
+
+// Functions that return promises of a certain type can also be annotated
+async function getFavoriteNumber(): Promise<number> {
+    return 26
+}
+
+
+// TypeScript can infer the type of some anonymous functions
+let cities = ["Miami", "Dallas", "Austin", "San Francisco"]
+
+cities.forEach((city) => console.log(city))
